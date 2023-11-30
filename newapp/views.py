c1561fbea49dffbc488747cc456b6d584e47fd14
Yasir Khan <yasirkhanone22@gmail.com>
2023-11-30 18:52:46 +0500

    views.py update

--- newapp/views.py
+++ newapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. Views are just functions.
 def home(request):
     return render(request, "index.html")
