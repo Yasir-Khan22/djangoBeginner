7de776e473751ab75be7cac66f9a564bcd83cd41
Yasir Khan <yasirkhanone22@gmail.com>
2023-12-18 22:58:40 +0500

    Model.py updated.

--- newapp/models.py
+++ newapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 # This is Customer model.
 class Customer(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=50)
     age = models.IntegerField()
     mobile = models.IntegerField()
 
